382c238be0d3bda49f1c9cba9eefe44b28d2716e
Matt Cobley <matt.cobley@moneyhub.com>
2023-12-12 15:06:23 +0000

    Adding rule adds it to the prompt sent to credal

--- ui/app/game/level1/dropzone.js
+++ ui/app/game/level1/dropzone.js
@@ -8,18 +8,15 @@ const defaultRules = [
   "Payments from lots of different sources to the same destination SortCode or AccountHash"
 ]
 
-export default function Dropzone({  options = [], messages = [], levelId, stepId = 1 }) {
-
-  const [rules, setRules] = useState(defaultRules)
-  const promptTemplate = `
-  Let's play a game.
+const getPromptTemplate = (rules) => `
+Let's play a game.
 
 We have a dataset of fake payments that we made up for the game. We're pretending that some of the payments were fraudulent. Think about what aspects of a set of payments might indicate fraudulent activity over time.
 
 Indicators of fraudulent payment activity may include:
- ${rules.map((rule) => `\n ${rule}`)}
+${rules.map((rule) => `\n ${rule}`)}
 
- In the next message, I will give you a dataset of payments, and I would like you to identify any payments that have a high probability of being fraudulent.
+In the next message, I will give you a dataset of payments, and I would like you to identify any payments that have a high probability of being fraudulent.
 
 For each potentially fraudulent payment, provide your reasoning for identifying. Where there are groups of payments that belong to the same pattern of activity, please allocate a unique number to each group.
 Please provide the results in table format, and for each payment, include:
@@ -35,16 +32,21 @@ Please provide the response in JSON format with no other information other than 
 
 The format of the input dataset is a tab delimited table of text. I'm aware that you're not designed for sophisticated data analysis, but please try anyway, as an experiment.
 
-  `
+`
+
+export default function Dropzone({  options = [], messages = [], levelId, stepId = 1 }) {
+
+  const [rules, setRules] = useState(defaultRules)
 
   const addRule = () => {
     setRules([...rules, ""])
   }
 
   const onClick = () => {
+    console.log(Array.from(document.getElementsByName("rule"))?.map((rule) => rule.value))
     document.getElementById("answer").innerHTML = ""
     const { promptGpt} = api()
-    return promptGpt(promptTemplate)
+    return promptGpt(getPromptTemplate(Array.from(document.getElementsByName("rule")).map((rule) => rule.value).filter((rule) => rule !== "")))
     .then(({session}) => promptGpt(document.getElementById("txs").value, session))
     .then((answer) => document.getElementById("answer").innerHTML = answer.data.replace(/\n/g, "<br>"))
   }
