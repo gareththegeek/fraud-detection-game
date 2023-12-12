ad5f8c9876d88931ae98088541927fd4652fbc77
Matt Cobley <matt.cobley@moneyhub.com>
2023-12-12 14:22:24 +0000

    Tweak page

--- ui/app/game/level1/dropzone.js
+++ ui/app/game/level1/dropzone.js
@@ -1,41 +1,10 @@
 import styles from '../game.module.css'
-import { useSelector, useDispatch } from 'react-redux'
-import Image from 'next/image'
-import { setAnswer } from '../../answerSlice'
-import { setText, resetText } from '../../textSlice'
 import api from '../../api'
+import { useState } from 'react'
 
-export default function Dropzone({ requestPanelImagePath, options = [], messages = [], levelId, stepId = 1 }) {
-  const dispatch = useDispatch()
-  const dragId = useSelector((state) => state.drag.dragId)
-  const answers = useSelector((state) => state.answer[levelId][stepId])
-  const allowDrop = (event) => {
-    event.preventDefault()
-  }
+export default function Dropzone({  options = [], messages = [], levelId, stepId = 1 }) {
 
-  const optionNames = options.map((option) => option.name)
-
-  const drop = (event) => {
-    event.preventDefault()
-
-    if (optionNames.includes(dragId)) {
-      dispatch(setAnswer({ levelId, stepId, answerName: dragId }))
-      dispatch(setText(messages.filter(({name}) => name == dragId)[0].message))
-    }
-    else {
-      dispatch(setText(messages.filter(({name}) => name == dragId)[0].message))
-    }
-  }
-
-  const click = (event) => {
-    event.preventDefault()
-    if (levelId === "level1" && stepId === 2) {
-      dispatch(setAnswer({ levelId, stepId, answerName: "consent" }))
-    }
-  }
-
-
-
+  const [txs, setTxs] = useState([])
   const rules = [
     "Repeated payments of similar amounts to the same destination SortCode or AccountHash in a short space of time",
     "Rejected payments",
@@ -67,6 +36,7 @@ The format of the input dataset is a tab delimited table of text. I'm aware that
   `
 
   const onClick = () => {
+    setTxs(document.getElementById("txs").value.split("\n"))
     const { promptGpt} = api()
     return promptGpt(promptTemplate).then((answer) => document.getElementById("answer").innerHTML = answer.data.replace(/\n/g, "<br>"))
   }
@@ -75,9 +45,6 @@ The format of the input dataset is a tab delimited table of text. I'm aware that
     <div
       className={styles.codeinternal}
       id="dropzone1"
-      onDrop={(e) => drop(e)}
-      onDragOver={(e) => allowDrop(e)}
-      onClick={(e) => click(e)}
     >
 
       {rules.map((rule) => {
@@ -87,6 +54,11 @@ The format of the input dataset is a tab delimited table of text. I'm aware that
           </div>
         )
       })}
+
+      <div>
+      <label>Paste in transaction data from spreadsheet</label>
+        </div>
+      <textarea id="txs" name="text" rows="8" cols="50" />
 
       <div>
         <button className={styles.getanswer} onClick={onClick}>Find fraud!</button>
