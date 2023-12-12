83eb356355cd0c8dc92d472519bd039c5fa8b9e9
Matt Cobley <matt.cobley@moneyhub.com>
2023-12-12 11:29:32 +0000

    Change server port to avoid clash

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ import { v4 as uuidv4 } from 'uuid';
 import express from 'express';
 import bodyParser from 'body-parser';
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
